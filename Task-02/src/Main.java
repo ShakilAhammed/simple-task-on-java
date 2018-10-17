8cbd4bc773e5a82d68eeb4420dc29a7ca59395f8
Shakil Ahammed <arshakilah@gmail.com>
2018-10-17 15:58:42 +0600

    updated

--- Task-02/src/Main.java
+++ Task-02/src/Main.java
@@ -3,6 +3,7 @@ import core.Constant;
 public class Main {
 
     public static void main(String[] args) {
-
+       ResourceContext rc = new ResourceContext();
+        System.out.println(rc.GetTotalMemory());
     }
 }
